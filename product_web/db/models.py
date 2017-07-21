6c636eddafd6badcca76b28af38f7decc1c75905
imzihuang <imzihuang@gmail.com>
2017-07-21 16:17:43 +0800

    add db

--- product_web/db/models.py
+++ product_web/db/models.py
@@ -3,6 +3,7 @@
 from sqlalchemy import Column, String
 from sqlalchemy.types import *
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 BaseModel = declarative_base()
 
@@ -17,9 +18,9 @@ DBSession = sessionmaker(bind=engine)
 
 class User(BaseModel):
     __tablename__ = 'user'
-    id = Column(String(36), primary_key=True,
+    id = Column(CHAR(36), primary_key=True,
                 default=lambda: str(uuid.uuid4()))
-    name = Column(CHAR(30))
+    name = Column(VARCHAR(30))
     pwd = Column(VARCHAR(20), default='888888', nullable=False)
     iage = Column(SMALLINT(), server_default='20')
     email = Column(VARCHAR(50))
@@ -30,6 +31,29 @@ class User(BaseModel):
     birthday_at = Column(DateTime)
     remark = Column(CHAR(100))
 
+class ProductType(BaseModel):
+    __tablename__ = 'producttype'
+    id = Column(CHAR(36), primary_key=True,
+                default=lambda: str(uuid.uuid4()))
+    name = Column(VARCHAR(30))
+    description = Column(VARCHAR(100))
+    created_at = Column(DateTime, default=lambda: utcnow(),nullable=False)
+    updated_at = Column(DateTime, default=lambda: utcnow(),
+                        nullable=True, onupdate=lambda: utcnow())
+
+class Product(BaseModel):
+    __tablename__ = 'product'
+    id = Column(CHAR(36), primary_key=True,
+                default=lambda: str(uuid.uuid4()))
+    name = Column(VARCHAR(30))
+    type_id = Column(CHAR(36))
+    description = Column(VARCHAR(100))
+    created_at = Column(DateTime, default=lambda: utcnow(),nullable=False)
+    updated_at = Column(DateTime, default=lambda: utcnow(),
+                        nullable=True, onupdate=lambda: utcnow())
+    product_type=relationship(ProductType, backref="product",
+                          foreign_keys=type_id,
+                          primaryjoin='Product.type_id == ProductType.id')
 
 def register_db():
     BaseModel.metadata.create_all(engine)
