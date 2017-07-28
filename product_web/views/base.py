f1606c772b4ff283cd3143fdc9ada1731fccc1ad
imzihuang <imzihuang@gmail.com>
2017-07-28 23:06:09 +0800

    updaet base

--- product_web/views/base.py
+++ product_web/views/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+from common.log_client import gen_log
+
 
 def entry_id_data_args(func):
     """
@@ -10,7 +12,9 @@ def entry_id_data_args(func):
     def __(torn_self):
         user_name = torn_self.get_secure_cookie('user_name')
         user_level = torn_self.get_secure_cookie('user_level')
-        if not user_name or user_level==1:
+        gen_log.info("entry:%s,%s"%(user_name, user_level))
+        #if not user_name or user_level=="1":
+        if not user_name:
             torn_self.redirect('/')
             return
         func(torn_self)
