ab1306d558774f7e91629e5a5970eca2f4ca3cf6
lin87786995 <imzihuang@gmail.com>
2017-08-01 00:22:18 +0800

    product_web/static/js/home.js

--- product_web/static/js/home.js
+++ product_web/static/js/home.js
@@ -12,20 +12,20 @@ $(document).ready(function(){
                 console.log(msg);
                 var str="";
                 for(var i=0;i<msg.data.length;i++){
-                	$('.countdown').downCount({
-						date: msg.data[i].count_down_at,
-						offset: +10
-					}, function () {
-						
-					});  
-					str+='<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="'+msg.data[i].img_path+
+					str_='<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="'+msg.data[i].img_path+
                    '"alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a>'+
                    msg.data[i].name+'</a></p><p class="color_gray">'+
                    msg.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>'+
                    msg.data[i].ori_price+'</a></span>&nbsp;&nbsp;<span class="color_gray"><a>'+
                    msg.data[i].con_price+'</a></span>&nbsp;&nbsp;<span class="color_gray_block">'+
                    msg.data[i].postage_price+'</span></p><a><span class="timedown">Start for you in：</span><ul class="countdown"><li> <span class="hours">00</span><span> :</span></li><li> <span class="minutes">00</span><span> :</span></li><li> <span class="seconds">00</span><span> </span></li></ul></a><p class="aboutHelp"><a>how to claim it?</a></p><div class="likeList"><span class="f_left"><a>share</a></span><span class="likecount">100</span><a ><img src="img/unlike.png"></a></div></div></div></div>'
-                    	
+                   $('.countdown').downCount({
+						date: msg.data[i].count_down_at,
+						offset: +10
+					}, function () {
+						
+					});  
+                   str=str+str_;
                    }   
                     $("#listPart").append(str);    
             },
