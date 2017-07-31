97113e643e83f6651529bf63a795844b18d7c575
imzihuang <imzihuang@gmail.com>
2017-07-31 21:44:54 +0800

    update ut

--- ut/mysql/pvpu_test.py
+++ ut/mysql/pvpu_test.py
@@ -1,13 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import unittest
+from datetime import datetime
 from logic import pvpu
 
 class SqlTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_pu_add(self):
+    def test_add(self):
+        _ = pvpu.pv_add("ip", "html")
         _ = pvpu.pu_add("ip", "html")
         self.assertTrue(_ > 0)
+
+    def test_get(self):
+        current_month = datetime.datetime.now().strftime('%Y-%m')+"-00 00:00:00"
+        current_date = datetime.datetime.now().strftime('%Y-%m-%d')+ " 00:00:00"
+        #_ = pvpu.get_pu("ip", "html", current_month)
+        _ = pvpu.get_pv("ip", "html")
 
 
 
