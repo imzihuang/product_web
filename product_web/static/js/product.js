42249af5b2e6f825fcfbfb30df78f07955ea46af
unknown <liyanping@jw.hongshu.com>
2017-08-01 19:07:41 +0800

    product_web/static/js/product.js

--- product_web/static/js/product.js
+++ product_web/static/js/product.js
@@ -18,10 +18,34 @@ $(document).ready(function(){
             async: false,
             data:data,
             success: function(msg) {
-              apply();
-              // if($("#listPart").val()==""){
-              //   $("#listPart").html("没有匹配项");
-              // }
+                   for(var i=0;i<msg.data.length;i++){
+          str_='<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="'+msg.data[i].img_path+
+                   '"alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a onclick=keyword_os()>'+
+                   msg.data[i].name+'</a></p><p class="color_gray">'+
+                   msg.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>'+
+                   msg.data[i].ori_price+'</a></span>&nbsp;&nbsp;<span class="color_gray"><a>'+
+                   msg.data[i].con_price+'</a></span>&nbsp;&nbsp;<span class="color_gray_block">'+
+                   msg.data[i].postage_price+'</span></p><a id="timedown"><span class="timedown">Start for you in：</span><ul class="countdown'+i+' countdown"><li><span class="days">00</span><span>日</span><span class="hours">00</span><span> :</span></li><li> <span class="minutes">00</span><span> :</span></li><li> <span class="seconds">00</span><span> </span></li></ul></a>'+
+                   '<p class="aboutHelp"><a>how to claim it?</a></p>'+
+                   '<div class="likeList"><span class="f_left"><a class="share share_face"><i class="fa fa-facebook areapen" title="Facebook"></i></a><a class="share share_twitter"><i class="fa fa-twitter areapen" title="twitter"></i></a><a class="share share_google"><i class="fa fa-google areapen" title="google"></i></a><a class="share share_envelope"><i class="fa fa-envelope areapen" title="envelope"></i></a></span>'+
+                   '<span class="likecount">100</span><a ><img src="img/unlike.png"></a></div></div></div></div>'
+                   
+                   str=str+str_;
+                   }   
+                    $("#listPart").append(str);  
+                    for(var j=0;j<msg.data.length;j++){
+                      var strname="";
+                      var strclass="";
+                      var strclass='.countdown'+j;
+                      strname=msg.data[j].count_down_at;
+                      console.log(strname);
+                      $(strclass).downCount({
+                      date: strname,
+                      offset: +10
+                    }, function () {
+                      $("#timedown").html('<span class="timedown">Start!</span>');
+                    }); 
+                  } 
             },
             error:function(){
                 console.log("error");
@@ -58,14 +82,14 @@ $(document).ready(function(){
                     	var strclass="";
                     	var strclass='.countdown'+j;
                     	strname=msg.data[j].count_down_at;
-                        console.log(strname);
+                      console.log(strname);
 	                    $(strclass).downCount({
-							date: strname,
-							offset: +10
-						}, function () {
-							$("#timedown").html('<span class="timedown">Start!</span>');
-						}); 
-					}  
+        							date: strname,
+        							offset: +10
+        						}, function () {
+        							$("#timedown").html('<span class="timedown">Start!</span>');
+        						}); 
+        					}  
             },
         });
     }
