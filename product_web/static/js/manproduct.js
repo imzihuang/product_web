a34e0f4ed5f292fd413c0f683ec47abecea3f381
lin87786995 <imzihuang@gmail.com>
2017-08-01 23:33:11 +0800

    product_web/static/js/manproduct.js

--- product_web/static/js/manproduct.js
+++ product_web/static/js/manproduct.js
@@ -122,14 +122,12 @@ $(function() {
 	$('#addInfo').click(function(){
 		$('input').val("");
 		$(".fileinput-upload-button").css("display","block");
-		// $(".fileinput-remove-button").click();
+		$(".fileinput-remove-button").click();
 		$(".rm_html").html("");
-		$("#file-zh_add").removeAttr("disabled");
-		$(".fileinput-remove-button").removeClass("hide");
-		$(".fileinput-remove-button").css("display","none");
-		$(".fileinput-remove-button").removeAttr("disabled");
-		$(".btn-file").removeAttr("disabled");
-		$(".fileinput-cancel-button").addClass("hide");
+		// $("#file-zh_add").removeAttr("disabled");
+		// $(".fileinput-remove-button").removeAttr("disabled");
+		// $(".btn-file").removeAttr("disabled");
+		// $(".fileinput-cancel-button").addClass("hide");
 		layerIndex=layer.open({
 		title:'添加产品',
 		  type: 1,
@@ -394,13 +392,13 @@ $(function() {
                 if($("#add_name").val()!=""&&$("#add_source").val()!=""&&$("#add_theme").val()!=""){
 			      productadd();
 			      $('input').val("");
-			      $(".fileinput-remove-button").click();
+			      // $(".fileinput-remove-button").click();
 	                    layer.closeAll();
-	                    // window.location.reload();
 						    layer.msg('添加成功', {
 						    	icon: 1,
 							    time: 800//2s后自动关闭
 							  });
+						    window.location.reload();
                 }   
 		
     });
