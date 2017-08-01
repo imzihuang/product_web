0e30be27a2d45ff002744c1298d73c5bedb4efe1
lin87786995 <imzihuang@gmail.com>
2017-08-01 22:54:24 +0800

    product_web/static/js/manproduct.js

--- product_web/static/js/manproduct.js
+++ product_web/static/js/manproduct.js
@@ -123,6 +123,8 @@ $(function() {
 		$('input').val("");
 		$(".fileinput-remove-button").click();
 		$(".rm_html").html("");
+		$("#file-zh_add").removeAttr("disabled");
+		$(".fileinput-remove-button").removeAttr("disabled");
 		$(".btn-file").removeAttr("disabled");
 		$(".fileinput-cancel-button").addClass("hide");
 		layerIndex=layer.open({
