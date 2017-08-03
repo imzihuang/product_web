2939d8827fbcb1321f2042cc8b4c70db93a3aec7
unknown <liyanping@jw.hongshu.com>
2017-08-03 15:42:40 +0800

    static/js/home.js

--- product_web/static/js/home.js
+++ product_web/static/js/home.js
@@ -96,7 +96,7 @@ function shareQZone(hrefName){
 function search(){console.log(22);
   var str="";
   var str_="";
-  for(var i=0;i<dataAll.data.length;i++){
+  for(var i=0;i<dataAll.data.length;i++){console.log(dataAll.data[i].name);
     if(dataAll.data[i].name==$("#homeSearch").val()){console.log(23);
       str_='<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="'+dataAll.data[i].img_path+
       '"alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a onclick=keyword_os("'+ dataAll.data[i].name +'")>'+
@@ -111,7 +111,7 @@ function search(){console.log(22);
       str=str+str_;
     }
   }
-  $("#listPart").attr("");console.log(24);
+  $("#listPart").html("");console.log(24);
   $("#listPart").append(str);  
 }
 
