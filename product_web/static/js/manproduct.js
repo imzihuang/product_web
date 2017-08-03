58f3bbece7ce664aca89785a9a76f5b7644ebcd2
lin87786995 <imzihuang@gmail.com>
2017-08-03 21:49:50 +0800

    product_web/static/js/manproduct.js

--- product_web/static/js/manproduct.js
+++ product_web/static/js/manproduct.js
@@ -70,7 +70,7 @@ $(function() {
                     data:data,
                     success: function(msg) {
                         // var data = JSON.parse(msg);
-                        if(mag.state==10)
+                        if(msg.state==10)
                         {
                         	window.location.href="/product/login.html";
                         }else{
