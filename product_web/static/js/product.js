beb1695bae946ff8919d32ba7b6578ae35bee1eb
unknown <liyanping@jw.hongshu.com>
2017-08-03 16:07:55 +0800

    static/js/product.js

--- product_web/static/js/product.js
+++ product_web/static/js/product.js
@@ -1,11 +1,11 @@
 var dataAll;
-console.log(dataAll);
-if($("#current_keyword").val()==""){console.log(1);
+var aboutOut=$(".track-sign-up").val();
+if($("#current_keyword").val()==""){
   apply();
 }
 else{
-  putkeyword();console.log(2);
-  
+  putkeyword();
+
 }
 function putkeyword(){
  var data = {
@@ -18,7 +18,6 @@ $.ajax({
   async: false,
   data:data,
   success: function(msg) {
-   console.log(msg);
    dataAll=msg;
    var str="";
    var str_="";
@@ -33,7 +32,7 @@ $.ajax({
      '<p class="aboutHelp"><a>how to claim it?</a></p>'+
      '<div class="likeList"><span class="f_left"><a class="share share_face"><i class="fa fa-facebook areapen" title="Facebook"></i></a><a class="share share_twitter"><i class="fa fa-twitter areapen" title="twitter"></i></a><a class="share share_google"><i class="fa fa-google areapen" title="google"></i></a><a class="share share_envelope"><i class="fa fa-envelope areapen" title="envelope"></i></a></span>'+
      '<span class="likecount">'+msg.data[i].like_count+'</span><a ><img src="img/unlike.png"id="'+msg.data[i].id+'"></a></div></div></div></div>'
-     
+
      str=str+str_;
    }   
    $("#listPart").append(str);  
@@ -42,7 +41,6 @@ $.ajax({
     var strclass="";
     var strclass='.countdown'+j;
     strname=msg.data[j].count_down_at;
-    console.log(strname);
     $(strclass).downCount({
       date: strname,
       offset: +10
@@ -63,64 +61,58 @@ error:function(){
         url:"/product/product_os",
         async: false,
         success: function(msg) {
-                // $("#listPart").find("div").remove();
-                console.log(msg);
-                dataAll=msg;
-                var str="";
-                var str_="";
-                for(var i=0;i<msg.data.length;i++){
-                 str_='<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="'+msg.data[i].img_path+
-                 '"alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a>'+
-                 msg.data[i].name+'</a></p><p class="color_gray">'+
-                 msg.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>'+
-                 msg.data[i].ori_price+'</a></span>&nbsp;&nbsp;<span class="color_gray"><a>'+
-                 msg.data[i].con_price+'</a></span>&nbsp;&nbsp;<span class="color_gray_block">'+
-                 msg.data[i].postage_price+'</span></p><a id="timedown"><span class="timedown">Start for you in：</span><ul class="countdown'+i+' countdown"><li><span class="days">00</span><span>日</span><span class="hours">00</span><span> :</span></li><li> <span class="minutes">00</span><span> :</span></li><li> <span class="seconds">00</span><span> </span></li></ul></a>'+
-                 '<p class="aboutHelp"><a>how to claim it?</a></p>'+
-                 '<div class="likeList"><span class="f_left"><a class="share share_face"><i class="fa fa-facebook areapen" title="Facebook"></i></a><a class="share share_twitter"><i class="fa fa-twitter areapen" title="twitter"></i></a><a class="share share_google"><i class="fa fa-google areapen" title="google"></i></a><a class="share share_envelope"><i class="fa fa-envelope areapen" title="envelope"></i></a></span>'+
-                 '<span class="likecount">'+msg.data[i].like_count+'</span><a ><img src="img/unlike.png"id="'+msg.data[i].id+'"></a></div></div></div></div>'
-                 
-                 str=str+str_;
-               }   
-               $("#listPart").append(str);  
-               for(var j=0;j<msg.data.length;j++){
-                 var strname="";
-                 var strclass="";
-                 var strclass='.countdown'+j;
-                 strname=msg.data[j].count_down_at;
-                 console.log(strname);
-                 $(strclass).downCount({
-                   date: strname,
-                   offset: +10
-                 }, function () {
-                   $("#timedown").html('<span class="timedown">Start!</span>');
-                 }); 
-               }  
-             },
-           });
+          dataAll=msg;
+          var str="";
+          var str_="";
+          for(var i=0;i<msg.data.length;i++){
+           str_='<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="'+msg.data[i].img_path+
+           '"alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a>'+
+           msg.data[i].name+'</a></p><p class="color_gray">'+
+           msg.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>'+
+           msg.data[i].ori_price+'</a></span>&nbsp;&nbsp;<span class="color_gray"><a>'+
+           msg.data[i].con_price+'</a></span>&nbsp;&nbsp;<span class="color_gray_block">'+
+           msg.data[i].postage_price+'</span></p><a id="timedown"><span class="timedown">Start for you in：</span><ul class="countdown'+i+' countdown"><li><span class="days">00</span><span>日</span><span class="hours">00</span><span> :</span></li><li> <span class="minutes">00</span><span> :</span></li><li> <span class="seconds">00</span><span> </span></li></ul></a>'+
+           '<p class="aboutHelp"><a>how to claim it?</a></p>'+
+           '<div class="likeList"><span class="f_left"><a class="share share_face"><i class="fa fa-facebook areapen" title="Facebook"></i></a><a class="share share_twitter"><i class="fa fa-twitter areapen" title="twitter"></i></a><a class="share share_google"><i class="fa fa-google areapen" title="google"></i></a><a class="share share_envelope"><i class="fa fa-envelope areapen" title="envelope"></i></a></span>'+
+           '<span class="likecount">'+msg.data[i].like_count+'</span><a ><img src="img/unlike.png"id="'+msg.data[i].id+'"></a></div></div></div></div>'
+
+           str=str+str_;
+         }   
+         $("#listPart").append(str);  
+         for(var j=0;j<msg.data.length;j++){
+           var strname="";
+           var strclass="";
+           var strclass='.countdown'+j;
+           strname=msg.data[j].count_down_at;
+           $(strclass).downCount({
+             date: strname,
+             offset: +10
+           }, function () {
+             $("#timedown").html('<span class="timedown">Start!</span>');
+           }); 
+         }  
+       },
+     });
     }
     //ajax
 
     $('.likeList a img').click(function(){
       var imgId=$(this).attr("id");
-      console.log(imgId);
       $(this).attr("src","img/like.png");
     //
     var data = {
       product_id:imgId
     };
-    console.log(data);
     $.ajax({
       type: "post",
       url:"/product/like_product",
       async: false,
       data:data,
       success: function(msg) {
-        console.log(msg);
         $(".likecount").html(msg.count);
       },
       error:function(){
-       
+
       }
     });
     //
@@ -135,13 +127,13 @@ error:function(){
           window.location.href='/product/login.html';
         },
         error:function(){
-         
+
         }
       });
-      
+
     }
     $(".product_name a").click(function(){
-      if($(".track-sign-up").val()!="Log Out"){console.log(111);
+      if(aboutOut=="Sign In"){
        window.location.href='/product/login.html';
      }
    });
