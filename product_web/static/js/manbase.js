43a0b967655f40a5fddd53a895490d1ea360328f
unknown <liyanping@jw.hongshu.com>
2017-08-04 10:08:34 +0800

    product_web/static/js/manbase.js

--- product_web/static/js/manbase.js
+++ product_web/static/js/manbase.js
@@ -1,8 +1,21 @@
 
 function down_base(method){
-    var dowen_a = document.createElement('a');
-    var down_url = "/product/excel_os?method="+method;
-    dowen_a.href = down_url;
+	var dowen_a = document.createElement('a');
+	var down_url = "/product/excel_os?method="+method;
+	dowen_a.href = down_url;
     //dowen_a.download = "proposed_file_name";
     dowen_a.click();
 }
+$('#editcompanyBtn').click(function(){
+	layerIndex=layer.open({
+		title:'编辑公司信息',
+		type: 1,
+		  skin: 'layui-layer-demo', //样式类名
+		  anim: 2,
+		  shadeClose: true, //开启遮罩关闭
+		  btn: ['确定', '取消'] ,//按钮
+		  content: $('#editcompany'),
+		  yes: function(){ 	
+		  }
+	});
+});
