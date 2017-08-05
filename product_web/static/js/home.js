1916964b2200cd6fe2a6e816af0e77fb50445116
unknown <liyanping@jw.hongshu.com>
2017-08-05 14:23:56 +0800

    product_web/static/js/home.js

--- product_web/static/js/home.js
+++ product_web/static/js/home.js
@@ -1,7 +1,8 @@
 apply();
-if(aboutOut=="signin.html"){
-  $(".product_name a").attr("onclick","change();return false");
-}
+// var aboutOut_href=$(".track-sign-up").attr("href");
+// if(aboutOut_href=="signin.html"){
+//   $(".product_name a").attr("onclick","change();return false");
+// }
 var aboutOut=$(".track-sign-up").val();
 var dataAll;
 function apply(){
@@ -12,19 +13,39 @@ function apply(){
     success: function(msg) {console.log(msg);
       dataAll=msg;
       var str="";
+      var aboutOut_href=$(".track-sign-up").attr("href");
+      if(aboutOut_href=="signin.html"){
+        $(".product_name a").attr("onclick","change();return false");
+        for(var i=0;i<msg.data.length;i++){
+         str_='<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="'+msg.data[i].img_path+
+         '"alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a onclick="change();return false">'+
+         msg.data[i].theme+'</a></p><p class="color_gray">'+
+         msg.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>'+
+         msg.data[i].ori_price+'￥</a></span>&nbsp;&nbsp;<span class="color_gray"><a>'+
+         msg.data[i].con_price+'￥</a></span>&nbsp;&nbsp;<span class="color_gray_block">postage:'+
+         msg.data[i].postage_price+'￥</span></p><a id="timedown"><span class="timedown">Start for you in：</span><ul class="countdown'+i+' countdown"><li><span class="days">00</span><span>日</span><span class="hours">00</span><span> :</span></li><li> <span class="minutes">00</span><span> :</span></li><li> <span class="seconds">00</span><span> </span></li></ul></a>'+
+         '<p class="aboutHelp"><a>how to claim it?</a></p>'+
+         '<div class="likeList"><img src="img/start.png" class="startimg"/><span class="likecount">'+(parseInt(msg.data[i].like_count)+parseInt(msg.data[i].like_add_count))+'</span><a ><img src="img/unlike.png" id="'+msg.data[i].id+'"></a></div>'+
+         '<div class="likeList"><span class="f_left"><a class="share share_face" onclick="shareFacebook('+"'"+window.location.href+"'"+')"><i class="fa fa-facebook areapen" title="Facebook"></i></a><a class="share share_twitter" onclick="shareQZone('+"'"+window.location.href+"'"+')"><i class="fa fa-twitter areapen" title="twitter"></i></a><a class="share share_google"><i class="fa fa-google areapen" title="google"></i></a></span></div></div></div></div></div>'
+         str=str+str_;
+       }   
+     }
+     else{
       for(var i=0;i<msg.data.length;i++){
-       str_='<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="'+msg.data[i].img_path+
-       '"alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a onclick=keyword_os("'+ msg.data[i].name +'")>'+
-       msg.data[i].theme+'</a></p><p class="color_gray">'+
-       msg.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>'+
-       msg.data[i].ori_price+'￥</a></span>&nbsp;&nbsp;<span class="color_gray"><a>'+
-       msg.data[i].con_price+'￥</a></span>&nbsp;&nbsp;<span class="color_gray_block">postage:'+
-       msg.data[i].postage_price+'￥</span></p><a id="timedown"><span class="timedown">Start for you in：</span><ul class="countdown'+i+' countdown"><li><span class="days">00</span><span>日</span><span class="hours">00</span><span> :</span></li><li> <span class="minutes">00</span><span> :</span></li><li> <span class="seconds">00</span><span> </span></li></ul></a>'+
-       '<p class="aboutHelp"><a>how to claim it?</a></p>'+
-       '<div class="likeList"><img src="img/start.png" class="startimg"/><span class="likecount">'+(parseInt(msg.data[i].like_count)+parseInt(msg.data[i].like_add_count))+'</span><a ><img src="img/unlike.png" id="'+msg.data[i].id+'"></a></div>'+
-       '<div class="likeList"><span class="f_left"><a class="share share_face" onclick="shareFacebook('+"'"+window.location.href+"'"+')"><i class="fa fa-facebook areapen" title="Facebook"></i></a><a class="share share_twitter" onclick="shareQZone('+"'"+window.location.href+"'"+')"><i class="fa fa-twitter areapen" title="twitter"></i></a><a class="share share_google"><i class="fa fa-google areapen" title="google"></i></a></span></div></div></div></div></div>'
-       str=str+str_;
-     }   
+         str_='<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="'+msg.data[i].img_path+
+         '"alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a onclick=keyword_os("'+ msg.data[i].name +'")>'+
+         msg.data[i].theme+'</a></p><p class="color_gray">'+
+         msg.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>'+
+         msg.data[i].ori_price+'￥</a></span>&nbsp;&nbsp;<span class="color_gray"><a>'+
+         msg.data[i].con_price+'￥</a></span>&nbsp;&nbsp;<span class="color_gray_block">postage:'+
+         msg.data[i].postage_price+'￥</span></p><a id="timedown"><span class="timedown">Start for you in：</span><ul class="countdown'+i+' countdown"><li><span class="days">00</span><span>日</span><span class="hours">00</span><span> :</span></li><li> <span class="minutes">00</span><span> :</span></li><li> <span class="seconds">00</span><span> </span></li></ul></a>'+
+         '<p class="aboutHelp"><a>how to claim it?</a></p>'+
+         '<div class="likeList"><img src="img/start.png" class="startimg"/><span class="likecount">'+(parseInt(msg.data[i].like_count)+parseInt(msg.data[i].like_add_count))+'</span><a ><img src="img/unlike.png" id="'+msg.data[i].id+'"></a></div>'+
+         '<div class="likeList"><span class="f_left"><a class="share share_face" onclick="shareFacebook('+"'"+window.location.href+"'"+')"><i class="fa fa-facebook areapen" title="Facebook"></i></a><a class="share share_twitter" onclick="shareQZone('+"'"+window.location.href+"'"+')"><i class="fa fa-twitter areapen" title="twitter"></i></a><a class="share share_google"><i class="fa fa-google areapen" title="google"></i></a></span></div></div></div></div></div>'
+         str=str+str_;
+       }   
+     }
+
      $("#listPart").append(str);
      var strclass="";  
      var strname="";
@@ -134,9 +155,9 @@ $(".product_name a").click(function(){
 });
 
 function change(){
-    layerIndex=layer.open({
-      title:'请先登录',
-      type: 1,
+  layerIndex=layer.open({
+    title:'请先登录',
+    type: 1,
               skin: 'layui-layer-demo', //样式类名
               anim: 2,
               shadeClose: true, //开启遮罩关闭
@@ -147,37 +168,37 @@ function change(){
               }
 
             });
-  };
-  function login(){
-    var data = {
-      user_name:$("#form-username").val(),
-      pwd:$("#form-password").val()
-    }
-    $.ajax({
-      type: "POST",
-      url:"/product/login",
-      async: false,
-      data:data,
-      success: function(msg) {
-        var data = JSON.parse(msg);
-        console.log(msg);
-        if(data.state==0){
-          layer.closeAll();
-          layer.msg('登录成功', {
-            icon: 1,
+};
+function login(){
+  var data = {
+    user_name:$("#form-username").val(),
+    pwd:$("#form-password").val()
+  }
+  $.ajax({
+    type: "POST",
+    url:"/product/login",
+    async: false,
+    data:data,
+    success: function(msg) {
+      var data = JSON.parse(msg);
+      console.log(msg);
+      if(data.state==0){
+        layer.closeAll();
+        layer.msg('登录成功', {
+          icon: 1,
                       time: 800//2s后自动关闭
                     });
-          window.location.reload();
-        }
-        else if(data.state==1){
-          $("#confirmMsg").html("用户名不存在！");
-        }
-        else{
-          $("#confirmpassword").html("密码错误！");
-        }     
-      },
-      error:function(){
-        console.log("error");
+        window.location.reload();
       }
-    });
-  }
+      else if(data.state==1){
+        $("#confirmMsg").html("用户名不存在！");
+      }
+      else{
+        $("#confirmpassword").html("密码错误！");
+      }     
+    },
+    error:function(){
+      console.log("error");
+    }
+  });
+}
