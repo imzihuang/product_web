0f9b07077912b0d562af7dc7e3963f81cb162acd
unknown <liyanping@jw.hongshu.com>
2017-08-05 14:15:43 +0800

    product_web/static/js/home.js

--- product_web/static/js/home.js
+++ product_web/static/js/home.js
@@ -1,4 +1,7 @@
 apply();
+if(aboutOut=="signin.html"){
+  $(".product_name a").attr("onclick","change();return false");
+}
 var aboutOut=$(".track-sign-up").val();
 var dataAll;
 function apply(){
@@ -129,3 +132,52 @@ $(".product_name a").click(function(){
    window.location.href='/product/login.html';
  }
 });
+
+function change(){
+    layerIndex=layer.open({
+      title:'请先登录',
+      type: 1,
+              skin: 'layui-layer-demo', //样式类名
+              anim: 2,
+              shadeClose: true, //开启遮罩关闭
+              btn:false,//按钮
+              content: $('#gologin'),
+              yes: function(){
+
+              }
+
+            });
+  };
+  function login(){
+    var data = {
+      user_name:$("#form-username").val(),
+      pwd:$("#form-password").val()
+    }
+    $.ajax({
+      type: "POST",
+      url:"/product/login",
+      async: false,
+      data:data,
+      success: function(msg) {
+        var data = JSON.parse(msg);
+        console.log(msg);
+        if(data.state==0){
+          layer.closeAll();
+          layer.msg('登录成功', {
+            icon: 1,
+                      time: 800//2s后自动关闭
+                    });
+          window.location.reload();
+        }
+        else if(data.state==1){
+          $("#confirmMsg").html("用户名不存在！");
+        }
+        else{
+          $("#confirmpassword").html("密码错误！");
+        }     
+      },
+      error:function(){
+        console.log("error");
+      }
+    });
+  }
