ab8963457dabc526d3cd3d2fd6a4b551e4494a31
unknown <liyanping@jw.hongshu.com>
2017-08-05 16:29:09 +0800

    product_web/static/js/home.js

--- product_web/static/js/home.js
+++ product_web/static/js/home.js
@@ -18,10 +18,10 @@ function apply(){
          str_='<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="'+msg.data[i].img_path+
          '"alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a onclick="change();return false">'+
          msg.data[i].theme+'</a></p><p class="color_gray">'+
-         msg.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>'+
-         msg.data[i].ori_price+'￥</a></span>&nbsp;&nbsp;<span class="color_gray"><a>'+
-         msg.data[i].con_price+'￥</a></span>&nbsp;&nbsp;<span class="color_gray_block">postage:'+
-         msg.data[i].postage_price+'￥</span></p><a id="timedown"><span class="timedown">Start for you in：</span><ul class="countdown'+i+' countdown"><li><span class="days">00</span><span>日</span><span class="hours">00</span><span> :</span></li><li> <span class="minutes">00</span><span> :</span></li><li> <span class="seconds">00</span><span> </span></li></ul></a>'+
+         msg.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>￥'+
+         msg.data[i].ori_price+'</a></span>&nbsp;&nbsp;<span class="color_gray"><a>￥'+
+         msg.data[i].con_price+'</a></span>&nbsp;&nbsp;<span class="color_gray_block">postage:￥'+
+         msg.data[i].postage_price+'</span></p><a id="timedown"><span class="timedown">Start for you in：</span><ul class="countdown'+i+' countdown"><li><span class="days">00</span><span>日</span><span class="hours">00</span><span> :</span></li><li> <span class="minutes">00</span><span> :</span></li><li> <span class="seconds">00</span><span> </span></li></ul></a>'+
          '<p class="aboutHelp"><a>how to claim it?</a></p>'+
          '<div class="likeList"><img src="img/start.png" class="startimg"/><span class="likecount">'+(parseInt(msg.data[i].like_count)+parseInt(msg.data[i].like_add_count))+'</span><a ><img src="img/unlike.png" id="'+msg.data[i].id+'"></a></div>'+
          '<div class="likeList"><span class="f_left"><a class="share share_face" onclick="shareFacebook('+"'"+window.location.href+"'"+')"><i class="fa fa-facebook areapen"></i></a><a class="share share_twitter" onclick="shareQZone('+"'"+window.location.href+"'"+')"><i class="fa fa-qq areapen"></i></a><a class="share share_google"><i class="fa fa-comments areapen"></i></a></span></div></div></div></div></div>'
@@ -33,10 +33,10 @@ function apply(){
        str_='<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="'+msg.data[i].img_path+
        '"alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a onclick=keyword_os("'+ msg.data[i].name +'")>'+
        msg.data[i].theme+'</a></p><p class="color_gray">'+
-       msg.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>'+
-       msg.data[i].ori_price+'￥</a></span>&nbsp;&nbsp;<span class="color_gray"><a>'+
-       msg.data[i].con_price+'￥</a></span>&nbsp;&nbsp;<span class="color_gray_block">postage:'+
-       msg.data[i].postage_price+'￥</span></p><a id="timedown"><span class="timedown">Start for you in：</span><ul class="countdown'+i+' countdown"><li><span class="days">00</span><span>日</span><span class="hours">00</span><span> :</span></li><li> <span class="minutes">00</span><span> :</span></li><li> <span class="seconds">00</span><span> </span></li></ul></a>'+
+       msg.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>￥'+
+       msg.data[i].ori_price+'</a></span>&nbsp;&nbsp;<span class="color_gray"><a>￥'+
+       msg.data[i].con_price+'</a></span>&nbsp;&nbsp;<span class="color_gray_block">postage:￥'+
+       msg.data[i].postage_price+'</span></p><a id="timedown"><span class="timedown">Start for you in：</span><ul class="countdown'+i+' countdown"><li><span class="days">00</span><span>日</span><span class="hours">00</span><span> :</span></li><li> <span class="minutes">00</span><span> :</span></li><li> <span class="seconds">00</span><span> </span></li></ul></a>'+
        '<p class="aboutHelp"><a>how to claim it?</a></p>'+
        '<div class="likeList"><img src="img/start.png" class="startimg"/><span class="likecount">'+(parseInt(msg.data[i].like_count)+parseInt(msg.data[i].like_add_count))+'</span><a ><img src="img/unlike.png" id="'+msg.data[i].id+'"></a></div>'+
        '<div class="likeList"><span class="f_left"><a class="share share_face" onclick="shareFacebook('+"'"+window.location.href+"'"+')"><i class="fa fa-facebook areapen"></i></a><a class="share share_twitter" onclick="shareQZone('+"'"+window.location.href+"'"+')"><i class="fa fa-qq areapen"></i></a><a class="share share_google"><i class="fa fa-comments areapen"></i></a></span></div></div></div></div></div>'
@@ -54,11 +54,6 @@ function apply(){
     }
   }
   $("#home_popular").append(pop);
-  setTime();
-  var times = setInterval(setTime, 1000);
-  function setTime(){
-    
-  }
   var strclass="";  
   var strname="";
   var current_obj=$(this);
@@ -132,10 +127,10 @@ function search(){
       str_='<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="'+dataAll.data[i].img_path+
       '"alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a onclick=keyword_os("'+ dataAll.data[i].name +'")>'+
       dataAll.data[i].theme+'</a></p><p class="color_gray">'+
-      dataAll.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>'+
-      dataAll.data[i].ori_price+'￥</a></span>&nbsp;&nbsp;<span class="color_gray"><a>'+
-      dataAll.data[i].con_price+'￥</a></span>&nbsp;&nbsp;<span class="color_gray_block">postage:'+
-      dataAll.data[i].postage_price+'￥</span></p><a id="timedown"><span class="timedown">Start for you in：</span><ul class="countdown'+i+' countdown"><li><span class="days">00</span><span>日</span><span class="hours">00</span><span> :</span></li><li> <span class="minutes">00</span><span> :</span></li><li> <span class="seconds">00</span><span> </span></li></ul></a>'+
+      dataAll.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>￥'+
+      dataAll.data[i].ori_price+'</a></span>&nbsp;&nbsp;<span class="color_gray"><a>￥'+
+      dataAll.data[i].con_price+'</a></span>&nbsp;&nbsp;<span class="color_gray_block">postage:￥'+
+      dataAll.data[i].postage_price+'</span></p><a id="timedown"><span class="timedown">Start for you in：</span><ul class="countdown'+i+' countdown"><li><span class="days">00</span><span>日</span><span class="hours">00</span><span> :</span></li><li> <span class="minutes">00</span><span> :</span></li><li> <span class="seconds">00</span><span> </span></li></ul></a>'+
       '<p class="aboutHelp"><a>how to claim it?</a></p>'+
       '<div class="likeList"><img src="img/start.png" class="startimg"/><span class="likecount">'+(parseInt(dataAll.data[i].like_count)+parseInt(dataAll.data[i].like_add_count))+'</span><a ><img src="img/unlike.png" id="'+dataAll.data[i].id+'"></a></div>'+
       '<div class="likeList"><span class="f_left"><a class="share share_face" onclick="shareFacebook('+"'"+window.location.href+"'"+')"><i class="fa fa-facebook areapen"></i></a><a class="share share_twitter" onclick="shareQZone('+"'"+window.location.href+"'"+')"><i class="fa fa-qq areapen"></i></a><a class="share share_google"><i class="fa fa-comments areapen"></i></a></span></div></div></div></div></div>'
