3bf754ac8483f5ecf59858087ec5085841731a65
unknown <liyanping@jw.hongshu.com>
2017-08-05 12:30:28 +0800

    product_web/static/js/home.js

--- product_web/static/js/home.js
+++ product_web/static/js/home.js
@@ -37,8 +37,8 @@ function apply(){
        $(strclass).downCount({
          date: strname,
          offset: +10
-       }, function () {console.log(0);
-         current_obj.parent().remove();
+       }, function () {console.log(current_obj.parent().html());
+         current_obj.parent().attr();
        }); 
      }  
       // }
