01fedbed357b2dc4c8bb2bc1762689984caf7c43
unknown <liyanping@jw.hongshu.com>
2017-08-05 12:43:36 +0800

    product_web/static/js/home.js

--- product_web/static/js/home.js
+++ product_web/static/js/home.js
@@ -37,7 +37,7 @@ function apply(){
        $(strclass).downCount({
          date: strname,
          offset: +10
-       }, function () {
+       }, function () {console.log($(this).html(""););
         $(this).html("");
        }); 
      }  
