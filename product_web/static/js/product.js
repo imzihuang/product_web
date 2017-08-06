c24c4acc0f9b20a93009073f22002dd17ea915c1
unknown <liyanping@jw.hongshu.com>
2017-08-06 13:33:14 +0800

    product_web/static/js/product.js

--- product_web/static/js/product.js
+++ product_web/static/js/product.js
@@ -104,7 +104,7 @@ function apply(){
           return "&postage:￥"+postage_price;
       };
       for(var i=0;i<msg.data.length;i++){
-       str_='<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="'+msg.data[i].img_path+
+       str_='<div class="col-sm-6 col-md-2"><div class="thumbnail"><img src="'+msg.data[i].img_path+
        '"alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a href="'+msg.data[i].links+'">'+
        msg.data[i].theme+'</a></p><p class="color_gray">'+
        msg.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>￥'+
@@ -189,7 +189,7 @@ function productsearch(){
   var tag=$("#productSearch").val();
   if(tag==""){
     for(var i=0;i<dataAll.data.length;i++){
-      str_='<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="'+dataAll.data[i].img_path+
+      str_='<div class="col-sm-6 col-md-2"><div class="thumbnail"><img src="'+dataAll.data[i].img_path+
       '"alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a href="'+dataAll.data[i].links+'">'+
       dataAll.data[i].theme+'</a></p><p class="color_gray">'+
       dataAll.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>￥'+
@@ -206,7 +206,7 @@ function productsearch(){
   }else{
     for(var i=0;i<dataAll.data.length;i++){
       if(dataAll.data[i].name.indexOf(tag)!=-1||dataAll.data[i].theme.indexOf(tag)!=-1||dataAll.data[i].description.indexOf(tag)!=-1){
-        str_='<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="'+dataAll.data[i].img_path+
+        str_='<div class="col-sm-6 col-md-2"><div class="thumbnail"><img src="'+dataAll.data[i].img_path+
         '"alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a href="'+dataAll.data[i].links+'">'+
         dataAll.data[i].theme+'</a></p><p class="color_gray">'+
         dataAll.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>￥'+
