92883d6f2f9289b98ee04aa1bbbea8581391be3b
unknown <liyanping@jw.hongshu.com>
2017-08-06 14:07:21 +0800

    product_web/static/js/home.js

--- product_web/static/js/home.js
+++ product_web/static/js/home.js
@@ -36,7 +36,7 @@ function apply(){
       if(msg.data[i].recommend==true)
       {
         pop_='<div class="col-sm-12 col-md-12"><div class="thumbnail"><img src="'+msg.data[i].img_path+
-        '"alt="通用的占位符缩略图"><div class="caption"><p><a href="'+msg.data[i].links+'">'+msg.data[i].theme+
+        '"alt="通用的占位符缩略图"><div class="caption"><p><a onclick=keyword_os("'+ msg.data[i].name +'")>'+msg.data[i].theme+
         '</a></p></div></div></div>'
         pop=pop+pop_;
       }
