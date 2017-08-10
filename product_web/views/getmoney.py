1056b1fea22bcf86fab73239e3303d4417f1c704
imzihuang <imaihuang@gmail.com>
2017-08-10 10:18:02 +0800

    update

--- product_web/views/getmoney.py
+++ product_web/views/getmoney.py
@@ -15,4 +15,4 @@ class GetMoneyHandler(tornado.web.RequestHandler):
         real_ip = self.request.headers.get("x-real-ip", self.request.headers.get("x-forwarded-for", ""))
         record_pv_pu(real_ip, "getmoney.html")
 
-        self.render('getmoney.html')
+        self.render('getmoney.html', user_name=self.get_secure_cookie('user_name'))
