96461849cfc0e962613887e7812d6ca8a7f36ceb
unknown <liyanping@jw.hongshu.com>
2017-08-10 15:40:09 +0800

    product_web/static/js/product.js

--- product_web/static/js/product.js
+++ product_web/static/js/product.js
@@ -12,7 +12,7 @@ else{
 if(aboutOut_href=="signin.html"){
   $(".product_name a").attr("onclick","change();return false");
   $(".likeList a img").attr("onclick","change();return false");
-  $(".thumbnail a").eq(0).attr("onclick","change();return false"); console.log(9);
+  $(".thumbnail").eq(0).attr("onclick","change();return false"); console.log(9);
 }
 function putkeyword(){
  var data = {
@@ -235,7 +235,7 @@ function productsearch(){
   var aboutOut_href=$(".track-sign-up").attr("href");
   if(aboutOut_href=="signin.html"){
     $(".likeList a img").attr("onclick","change();return false");
-    $(".thumbnail a").eq(0).attr("onclick","change();return false");
+    $(".thumbnail").eq(0).attr("onclick","change();return false");
   }
   for(var j=0;j<dataAll.data.length;j++){
    var strname="";
