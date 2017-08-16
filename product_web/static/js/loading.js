c7270aa514eaaecec58de2afe9a26bd1d73ddff7
unknown <liyanping@jw.hongshu.com>
2017-08-16 17:51:08 +0800

    product_web/static/js/loading.js

--- product_web/static/js/loading.js
+++ product_web/static/js/loading.js
@@ -10,7 +10,7 @@ var _LoadingHtml = '<div id="loadingDiv" style="position:absolute;left:0;width:1
 '5px; background: #000;color: #fff; font-family:\'Microsoft YaHei\';">loading...</div></div>';
 //呈现loading效果
 document.write(_LoadingHtml);
-$("#loadingDiv").parents().css("overflow","hidden");console.log($("#loadingDiv").parents().html());
+$("#loadingDiv").parents().find("body").css("overflow","hidden");console.log($("#loadingDiv").parents().html());
 //window.onload = function () {
 //    var loadingMask = document.getElementById('loadingDiv');
 //    loadingMask.parentNode.removeChild(loadingMask);
