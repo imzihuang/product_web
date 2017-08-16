9dc31b1b49b61803c0a8ed20fc9d8007abc4a378
imzihuang <imaihuang@gmail.com>
2017-08-16 10:16:50 +0800

    update

--- product_web/ser_email/ser_email.py
+++ product_web/ser_email/ser_email.py
@@ -82,10 +82,11 @@ def gmail_send_email(to_email, message, subject):
             service_smtp.starttls()
             # login with username & password
             service_smtp.login(gmail_email_user, gmail_email_pwd)
+            msg = MIMEText(message)
             msg['From'] = gmail_email_user
             msg['To'] = to_email
             msg['Subject'] = subject
-            service_smtp.sendmail(from_addr, to_email, message)
+            service_smtp.sendmail(gmail_email_user, to_email, msg.as_string())
             return True
         except smtplib.SMTPException, e:
             gen_log.error("send email error:%r" % e)
