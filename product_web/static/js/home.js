925c12fb0207d64e3a838b3239a4a18958a7d35a
unknown <liyanping@jw.hongshu.com>
2017-08-16 19:01:49 +0800

    product_web/static/js/home.js

--- product_web/static/js/home.js
+++ product_web/static/js/home.js
@@ -24,7 +24,7 @@ function apply(){
       };
       var aboutOut_href=$(".track-sign-up").attr("href");
       for(var i=0;i<msg.data.length;i++){
-       str_='<div class="col-sm-6 col-md-2"><div class="thumbnail"><img src="'+msg.data[i].img_path+'" val="'+ msg.data[i].name +'" onclick=keyword_os(this) alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a val="'+ msg.data[i].name +'" onclick=keyword_os(this)>'+
+       str_='<div class="col-sm-6 col-md-2"><div class="thumbnail"><img src="'+msg.data[i].img_path+'" class="phonePleft" val="'+ msg.data[i].name +'" onclick=keyword_os(this) alt="通用的占位符缩略图"><div class="caption text-left phonePright"><p class="product_name"><a val="'+ msg.data[i].name +'" onclick=keyword_os(this)>'+
        msg.data[i].theme+'</a></p><p class="color_gray by">'+
        msg.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>￥'+
        msg.data[i].con_price+'</a></span>&nbsp;&nbsp;<span class="color_gray"><a>￥'+
@@ -143,7 +143,7 @@ function search(){
   };
   if(tag==""){
     for(var i=0;i<dataAll.data.length;i++){
-      str_='<div class="col-sm-6 col-md-2"><div class="thumbnail"><img src="'+dataAll.data[i].img_path+'" val="'+ dataAll.data[i].name +'" onclick=keyword_os(this) alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a href="'+dataAll.data[i].links+'">'+
+      str_='<div class="col-sm-6 col-md-2"><div class="thumbnail"><img src="'+dataAll.data[i].img_path+'" class="phonePleft" val="'+ dataAll.data[i].name +'" onclick=keyword_os(this) alt="通用的占位符缩略图"><div class="caption text-left phonePright"><p class="product_name"><a href="'+dataAll.data[i].links+'">'+
       dataAll.data[i].theme+'</a></p><p class="color_gray by">'+
       dataAll.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>￥'+
       dataAll.data[i].con_price+'</a></span>&nbsp;&nbsp;<span class="color_gray"><a>￥'+
@@ -161,7 +161,7 @@ function search(){
   }else{
     for(var i=0;i<dataAll.data.length;i++){
       if(dataAll.data[i].name.indexOf(tag)!=-1||dataAll.data[i].theme.indexOf(tag)!=-1||dataAll.data[i].description.indexOf(tag)!=-1){
-        str_='<div class="col-sm-6 col-md-2"><div class="thumbnail"><img src="'+dataAll.data[i].img_path+'" onclick=keyword_os(this) alt="通用的占位符缩略图"><div class="caption text-left"><p class="product_name"><a href="'+dataAll.data[i].links+'">'+
+        str_='<div class="col-sm-6 col-md-2"><div class="thumbnail"><img src="'+dataAll.data[i].img_path+'" class="phonePleft" onclick=keyword_os(this) alt="通用的占位符缩略图"><div class="caption text-left phonePright"><p class="product_name"><a href="'+dataAll.data[i].links+'">'+
         dataAll.data[i].theme+'</a></p><p class="color_gray by">'+
         dataAll.data[i].source+'</p><p class="howmuch"><span class="color_red"><a>￥'+
         dataAll.data[i].con_price+'</a></span>&nbsp;&nbsp;<span class="color_gray"><a>￥'+
