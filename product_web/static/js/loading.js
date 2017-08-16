58c7b2c7da9e4321e7551af14dcec5c681c2fac6
unknown <liyanping@jw.hongshu.com>
2017-08-16 15:43:58 +0800

    product_web/static/js/loading.js

--- product_web/static/js/loading.js
+++ product_web/static/js/loading.js
@@ -6,8 +6,8 @@ var _LoadingTop = _PageHeight > 61 ? (_PageHeight - 61) / 2 : 0,
     _LoadingLeft = _PageWidth > 215 ? (_PageWidth - 215) / 2 : 0;
 //在页面未加载完毕之前显示的loading Html自定义内容
 var _LoadingHtml = '<div id="loadingDiv" style="position:absolute;left:0;width:100%;height:' + _PageHeight + 'px;top:0;background:#000;opacity:0.4;filter:alpha(opacity=80);z-index:10000;">'+
-'<div style="position: absolute; cursor1: wait; left: ' + _LoadingLeft + 'px; top:' + _LoadingTop + 'px; width: auto; height: 57px; line-height: 57px; padding-left: 10px; padding-right: '+
-'5px; background: #000 url(/Content/loading.gif) no-repeat scroll 5px 10px;color: #fff; font-family:\'Microsoft YaHei\';">loading...</div></div>';
+'<div style="position: absolute; cursor1: wait; left: ' + _LoadingLeft + 'px; top:' + _LoadingTop + 'px; width: 100%; height: 57px;margin:0 auto;line-height: 57px;font-size:16px;'+
+'background: #000 url(/Content/loading.gif) no-repeat scroll 5px 10px;color: #fff; font-family:\'Microsoft YaHei\';">loading...</div></div>';
 //呈现loading效果
 document.write(_LoadingHtml);
 
