485d8b30bc9961e5b2221b7229426f31c6f30d84
unknown <liyanping@jw.hongshu.com>
2017-08-17 16:19:50 +0800

    product_web/static/js/home.js

--- product_web/static/js/home.js
+++ product_web/static/js/home.js
@@ -3,7 +3,7 @@ var aboutOut=$(".track-sign-up").val();
 var dataAll;
 var aboutOut_href=$(".track-sign-up").attr("href");
 if(aboutOut_href=="signin.html"){
-  $(".likeList a img").attr("onclick","change();return false");console.log(11);
+  $(".likeList a img").attr("width","50px");console.log(11);
 }
 
 function apply(){
