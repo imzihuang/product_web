625e139d6ccd1c784cf0d9a11ee36330357d2f19
unknown <liyanping@jw.hongshu.com>
2017-08-17 17:35:44 +0800

    product_web/static/js/home.js

--- product_web/static/js/home.js
+++ product_web/static/js/home.js
@@ -244,8 +244,8 @@ function signin(){
 }
 
 //点赞
-$(".startimg").click(function(){console.log(3);
-  var imgId=$(this).attr("id");
+$(".likeList a img").click(function(){console.log(3);
+  var imgId = $(this).attr("id");
   var current_obj = $(this);
   current_obj.attr("src","img/like.png");
   var data = {
